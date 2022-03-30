430b1193dec436abfabba0750ebdd87629df5f98
Lydia <75390984+iydia@users.noreply.github.com>
2022-03-30 14:48:07 +0000

    Completed processing task

--- Sketch.java
+++ Sketch.java
@@ -2,35 +2,67 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
   /**
-   * Called once at the beginning of execution, put your size all in this method
+   * Assignment: 5.5 - Processing Task 4 - Loops
+   * Author: Lydia He
+   * Date: Tue Mar 29, 2022
+   * Task: Using loop patterns to draw sequences of objects
    */
+  
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    width = 600;
+    height = 600;
+    size(width, height);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(210, 255, 173);
-  }
-
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
+    background(255, 255, 255);
   }
   
-  // define other methods down here.
+  public void draw() {
+    // quadrant 1
+    stroke(0);
+    strokeWeight(1);
+    for (int lineX = 0; lineX <= width*100/222; lineX += width/20) {
+      line(lineX, 0, lineX, height/2);
+    }
+    for (int lineY = 0; lineY <= height/2; lineY += height/20) {
+      line(0, lineY, width/2, lineY);
+    }
+    
+    // quadrant 2
+    for (int circleY = height/10; circleY < height*100/210; circleY += height*100/1176) {
+      for (int circleX = width*100/170; circleX < width*100/102; circleX += width*100/1176) {
+        fill(212, 15, 125);
+        ellipse(circleX, circleY, width*100/1904, height*100/1904);
+      }
+    }
+
+    // quadrant 3
+    int hue = 0;
+    for (int gradX = 0; gradX <= height/2; gradX++) {
+      stroke(hue);
+      line(gradX, height, gradX, height/2);
+      hue += 1.9;
+    }
+    
+    // quadrant 4
+    stroke(0);
+    int newWidth = width*3/4;
+    int newHeight = height*3/4;
+    translate(newWidth, newHeight);
+    for (int i = 0; i < 5; i++) {
+      // petals
+      stroke(0);
+      fill(220, 93, 52);
+      ellipse(0, 0, width*1/22, height*1/3);
+      rotate(radians(45));
+
+      //core
+      stroke(37, 109, 45);
+      fill(37, 109, 45);
+      ellipse(0, 0, width*1/11, height*1/11);
+    }
+  }
 }
